9a84462d69352069f947d0f023c79f751f2b24b6
Gabriel Cilico <gabrielcilico@gmail.com>
2021-06-16 20:44:15 -0300

    add subscription controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import voluntaryController from './voluntary/voluntary-controller'
 import organizationController from './organization/organization-controller'
+import inviteController from './invite/invite-controller'
+import subscriptionController from './subscription/subcription-controller'
 
 const routes = Router()
 
@@ -14,5 +16,16 @@ routes.put('/organization', organizationController.update)
 routes.get('/organization', organizationController.getAll)
 routes.get('/organization/:id', organizationController.getById)
 routes.get('/organization/category/:category', organizationController.getByCategory)
+
+routes.post('/invite', inviteController.invite)
+routes.post('/invite/accept', inviteController.accept)
+routes.post('/invite/cancel', inviteController.cancel)
+routes.post('/invite/reject', inviteController.reject)
+
+routes.post('/invite', subscriptionController.invite)
+routes.post('/invite/accept', subscriptionController.accept)
+routes.post('/invite/cancel', subscriptionController.cancel)
+routes.post('/invite/reject', subscriptionController.reject)
+
 
 export { routes }
---
+++ src/subscription/subcription-controller.ts
@@ -0,0 +1,97 @@
+import { Subscription } from "./subscription-model";
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Organization } from "../organization/organization-model";
+import { Voluntary } from "../voluntary/voluntary-model";
+
+class SubscriptionController {
+
+  async subscription(req: Request, res: Response) {
+    let repository = getRepository(Subscription)
+    let { idVoluntary, idOrganization } = req.body
+    let subscriptionExists = await repository.findOne({ where: { 
+      id_organization: idOrganization, 
+      id_voluntary: idVoluntary,
+      status: 'pending'
+    }})
+
+    if (subscriptionExists) {
+      return res.status(409).send({ message: "this subscription already exists and is pending"})
+    }
+    
+    let orgRepository = getRepository(Organization)
+    let organization = await orgRepository.findOne({ where: {id: idOrganization}})
+    if (!organization) {
+      return res.status(404).send({ message: "organization not found"})
+    }
+    
+    let volRepository = getRepository(Voluntary)
+    let voluntary = await volRepository.findOne({ where: {id: idVoluntary}})
+    if (!voluntary) {
+      return res.status(404).send({ message: "voluntary not found"})
+    }
+
+    const subscription = repository.create({organization, voluntary, status: 'pending'})
+    await repository.save(subscription)
+    return res.status(201).send(subscription)
+  }
+
+  async accept(req: Request, res: Response) {
+    let repository = getRepository(Subscription)
+    let { idVoluntary, idOrganization } = req.body
+    const subscription = await repository.findOne({ where: { 
+      id_organization: idOrganization, 
+      id_voluntary: idVoluntary,
+      status: 'pending'
+    }})
+
+    if (!subscription) {
+      return res.status(404).send({ message: "subscription not found"})
+    }
+
+    subscription.status = 'accepted'
+
+    // TODO: Deve gerar um registro de trabalho com status em aberto
+
+    await repository.save(subscription)
+    return res.status(200).send(subscription)
+  }
+
+  async reject(req: Request, res: Response) {
+    let repository = getRepository(Subscription)
+    let { idVoluntary, idOrganization } = req.body
+    const subscription = await repository.findOne({ where: { 
+      id_organization: idOrganization, 
+      id_voluntary: idVoluntary,
+      status: 'pending'
+    }})
+
+    if (!subscription) {
+      return res.status(404).send({ message: "subscription not found"})
+    }
+
+    subscription.status = 'rejected'
+    await repository.save(subscription)
+    return res.status(200).send(subscription)
+  }
+
+  async cancel(req: Request, res: Response) {
+    let repository = getRepository(Subscription)
+    let { idVoluntary, idOrganization } = req.body
+    const subscription = await repository.findOne({ where: { 
+      id_organization: idOrganization, 
+      id_voluntary: idVoluntary,
+      status: 'pending'
+    }})
+
+    if (!subscription) {
+      return res.status(404).send({ message: "subscription not found"})
+    }
+
+    subscription.status = 'cancelled'
+    await repository.save(subscription)
+    return res.status(200).send(subscription)
+  }
+}
+
+export default new SubscriptionController()
