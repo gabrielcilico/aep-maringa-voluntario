89171b3adffd85b016c01e0ed35bd49debab9015
Gabriel Cilico <gabrielcilico@gmail.com>
2021-06-15 22:37:27 -0300

    add database url

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,14 @@
 module.exports = {
   "type": "postgres",
-  "host": process.env.DATABASE_HOST,
-  "port": process.env.DATABASE_PORT,
-  "username": process.env.DATABASE_USER,
-  "password": process.env.DATABASE_PASS,
-  "database": process.env.DATABASE_NAME,
-  "ssl": {
-    "ca": process.env.SSL_CERT 
-  },
+  "url": process.env.DATABASE_URL,
+  // "host": process.env.DATABASE_HOST,
+  // "port": process.env.DATABASE_PORT,
+  // "username": process.env.DATABASE_USER,
+  // "password": process.env.DATABASE_PASS,
+  // "database": process.env.DATABASE_NAME,
+  // "ssl": {
+  //   "ca": process.env.SSL_CERT 
+  // },
   "entities": [
      process.env.ENTITIES_PATH
   ],
