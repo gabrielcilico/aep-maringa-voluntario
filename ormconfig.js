d5eeab05bc18d6bf9f4272d62c5d2ac7cd052ff8
Gabriel Cilico <gabrielcilico@gmail.com>
2021-06-15 22:26:00 -0300

    add SSL CERTIFIED

--- ormconfig.js
+++ ormconfig.js
@@ -5,6 +5,9 @@ module.exports = {
   "username": process.env.DATABASE_USER,
   "password": process.env.DATABASE_PASS,
   "database": process.env.DATABASE_NAME,
+  "ssl": {
+    "ca": process.env.SSL_CERT 
+  },
   "entities": [
      process.env.ENTITIES_PATH
   ],
