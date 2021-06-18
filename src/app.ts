0d28edc8c60f9483825152ff6b258f788cacae34
Gabriel Cilico <gabrielcilico@gmail.com>
2021-06-17 23:21:02 -0300

    initialize swaagger

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import express from 'express'
 import { routes } from './routes'
 import cors from 'cors'
 import "./database/connect"
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 export { app }
